7862a23fb2b8d7bdcef3a4064c3239ba03c1eb96
Mariano Di Bacco <mdibacco5973@users.noreply.github.com>
2026-04-29 21:54:10 -0300

    ajustes

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,16 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME
+} = process.env;
+
+const DATABASE_URL = `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
